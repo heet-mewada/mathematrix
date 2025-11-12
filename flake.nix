65f3e5037b5c726593a41a924157dfe82698f817
heet-mewada <heetmewadapersonal@gmail.com>
2025-11-12 10:39:31 +0700

    flake initialized

---
+++ flake.nix
@@ -0,0 +1,32 @@
+{
+  description = "Dev shell";
+
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+  outputs = { self, nixpkgs }: {
+    devShells.x86_64-linux.default = let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs { inherit system; };
+    in pkgs.mkShell {
+      name = "dev shell";
+
+      packages = with pkgs; [
+        python312
+        poetry
+        zlib
+        gcc
+      ];
+
+      # Export LD_LIBRARY_PATH so Python (NumPy, etc.) can see system libs
+      LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath [
+        pkgs.zlib
+        pkgs.gcc.cc.lib
+      ];
+
+      shellHook = ''
+        echo "installation completed"
+      '';
+    };
+  };
+}
+
